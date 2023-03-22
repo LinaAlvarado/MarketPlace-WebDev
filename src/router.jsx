a66a4f92a84588b77e8924127acc436b90ab678c
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2023-03-22 12:11:22 -0500

    Router

---
+++ src/router.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+<BrowserRouter>
+        <Routes>
+          <Route path="/" element={}/>
+          <Route path="product" element={} />
+       </Routes>
+</BrowserRouter>
